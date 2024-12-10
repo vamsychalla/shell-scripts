5ee5fc6b6e930a28272ff37c14200fa0021d44cc
vamsychalla <vamsichalla575@gmail.com>
2024-12-10 12:01:02 +0530

    Passing three perameters

---
+++ parameters2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#declaring variable for passing parameters
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+#Refering Variables
+echo "$PERSON1:: HI $PERSON2 and $PERSON3, HOW ARE YOU?"
+echo "$PERSON3:: Hello $PERSON1, I am good, Thanks. I think $PERSON2 is not available to join this call"
