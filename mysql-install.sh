fa44a760900f8763755a2bcf716957b38a0a05ac
vamsychalla <vamsichalla575@gmail.com>
2024-12-10 22:18:30 +0530

    Passing three perameters

--- mysql-install.sh
+++ mysql-install.sh
@@ -5,9 +5,23 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then 
     echo "you need to be run this script with super user"
+    exit 1
 else
     echo "srcipt will be execute and will check netstat"
  fi
 
  dnf install mysql -y
- 
+if [ $? ne 0 ]
+then 
+    echo "installation of mysql id failed "
+    exit 1
+else
+    echo "installation is success"
+
+dnf install git -y
+if [ $? ne 0 ]
+then 
+    echo "installation of mysql id failed "
+    exit 1
+else
+    echo "installation is success"
