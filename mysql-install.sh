23538375f2b920327f26d997483dda491134f5fc
vamsychalla <vamsichalla575@gmail.com>
2024-12-10 21:58:27 +0530

    Passing three perameters

---
+++ mysql-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [$USER -ne 0 ]
+then 
+    echo "you need to be run this script with super user"
+else
+    echo "srcipt will be execute and will check netstat"
+ fi
