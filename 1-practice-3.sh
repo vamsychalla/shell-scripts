76af4b414d3ce0b0a73f9b1e54da62113a4b8c8e
vamsychalla <vamsichalla575@gmail.com>
2024-12-12 11:47:00 +0530

    hello practice

--- 1-practice-3.sh
+++ 1-practice-3.sh
@@ -4,8 +4,8 @@ echo "hello worlds"
 
 #Declare a variables
 
-PERSON1=RAVI
-PERSON2=RAJ
+PERSON1=$1
+PERSON2=$2
 
-echo "$PERSON1:: Hi $PERSON2, how are you"
-echo "$PERSON2:: HI $PERSON1, I AM GOOD thanks what about you"
+echo "$PERSON1:: HI $PERSON2, How are you"
+echo "$PERSON2:: HI $PERSON1, I am good thanks"
