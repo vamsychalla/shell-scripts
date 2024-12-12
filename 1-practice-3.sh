3471cc75f944f918327b7243904bc40943581bdb
vamsychalla <vamsichalla575@gmail.com>
2024-12-12 12:08:12 +0530

    hello practice

--- 1-practice-3.sh
+++ 1-practice-3.sh
@@ -8,6 +8,7 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then 
     echo "this script will not work, kindly run this script as a root user then only it will work"
+    exit 1
     
 else
     echo "this script will work"
